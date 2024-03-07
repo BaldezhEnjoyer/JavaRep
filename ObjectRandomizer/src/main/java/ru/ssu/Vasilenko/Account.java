b98ebc3194509a714adf9e397f1bf64b7956abe5
BaldezhEnjoyer <138496816+BaldezhEnjoyer@users.noreply.github.com>
2024-03-07 21:50:21 +0400

    Add files via upload

---
+++ ObjectRandomizer/src/main/java/ru/ssu/Vasilenko/Account.java
@@ -0,0 +1,22 @@
+package ru.ssu.Vasilenko;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.EqualsAndHashCode;
+
+
+enum Gender {
+    Male,
+    Female;
+}
+
+@EqualsAndHashCode
+@Setter
+@Getter
+@ToString
+class Account {
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private Gender gender;
+}
---
+++ ObjectRandomizer/src/main/java/ru/ssu/Vasilenko/Main.java
@@ -0,0 +1,16 @@
+package ru.ssu.Vasilenko;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+    private static final RandomObjectGenerator randomObjectGenerator = new RandomObjectGenerator();
+
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InstantiationException, InvocationTargetException {
+        Account randomAccount1 = randomObjectGenerator.<Account>fullRandomObjectGenerate(Account.class);
+        Account man = new Account();
+        man.setFirstName("MAN");
+        Account randomAccount2 = randomObjectGenerator.<Account>nullFieldsRandomObjectGenerate(man, Account.class);
+        System.out.println(randomAccount2.toString());
+    }
+}
+
